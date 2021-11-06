d211a825c546320dfbc1cec53475ea3d0e9c6e41
Emmanuel Ouzan <Emmanuel.ouzan@gmail.com>
2021-11-06 20:38:18 +0200

    With index_of()

--- For_Loops.js
+++ For_Loops.js
@@ -1,18 +1,16 @@
     // For loops
 
     // input from the user a userString and a choice
+let userString = prompt("Enter a string");
+    let userinput = prompt(`
+    What do you want to do brother?
+    [1] - for printing a letter
+    [2] - for printing the userString reversed
+    [3] - Checks if the userString is a palindrome
+    [4] - Prints if the userString contains a number
+    `
+    );
 
-    // let userinput = prompt(`
-    // What do you want to do brother?
-    // [1] - for printing a letter
-    // [2] - for printing the userString reversed
-    // [3] - Checks if the userString is a palindrome
-    // [4] - Prints if the userString contains a number
-    // `
-    // )
-
-    let userString = 'Emmanuel';
-    let userinput = '5';
     let new_string = "";
     let exit = true;
 while(exit){
@@ -54,16 +52,13 @@ while(exit){
                     break;
                 }
                 case '5':
-                    console.log("Now, I will count the encourences of the char in the string");
-                    let char = 'm';
-                    let counter = 0;
-                    for (let i = 0; i < userString.length - 1; i++) {
-                        if (char == userString[i]) {
-                            counter++;
-                        }
+                    let charToFind = prompt("Which char do you want to find in the string?");
+                    let count = 0;
+                    let position = 0;
+                    while(userString.indexOf(charToFind,position) !== -1){
+                        count++;
+                        position = userString.indexOf(charToFind, position) + 1;
                     }
-                    console.log(`The number is appearing ${counter} times`)
-
                     break;
                 case null:
                 case '6':
