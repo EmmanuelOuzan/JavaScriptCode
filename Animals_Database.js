6766e5023616a1e733551536c5ba0169bb33f41b
Emmanuel Ouzan <Emmanuel.ouzan@gmail.com>
2021-11-07 21:24:10 +0200

    UnderWork

--- Animals_Database.js
+++ Animals_Database.js
@@ -3,9 +3,21 @@ let txt1 = "Dog12, CAT3, LiOn7, DolphiN11, fish6".toLowerCase();
 let txt2 = "PIG17, bear29, BiRd8, SNAKE39, donkey14".toLowerCase();
 txt1 = txt1.match(/[a-zA-Z]+|[0-9]+/g);
 txt2 = txt2.match(/[a-zA-Z]+|[0-9]+/g);
-console.log(txt1)
-console.log(txt2)
+const keys = [];
+const values = [];
+for(let i = 0; i < (txt1.length + txt2.length); i++){
+    keys[i] = txt1[i]
+    values[i] = txt1[i]
+}
+console.log(keys)
+console.log(values)
 
+
+const animals_by_name = [];
+for(let i = 0; i < txt1.length; i++){
+  console.log(txt1[i]);
+  animals_by_name.push(txt1[i])
+}
 
 // let first_array = txt1.split(", ");
 // let second_array = txt2.split(", ");
