421fd11a0c345a9793d9c949185f909f9e694a02
Emmanuel Ouzan <Emmanuel.ouzan@gmail.com>
2021-11-10 23:06:06 +0200

    Some Upgrades 11.10

--- Booleans/Comparison_With_Operators.js
+++ Booleans/Comparison_With_Operators.js
@@ -1,10 +1,15 @@
-// Priority of the booleans
 let username = prompt("Enter username")
 let firstName = prompt("FirstName")
 let familiyName = prompt("Enter Familiy Name")
 
-console.log("Hello! " + (username || firstName || familiyName || "Annoymous!"))
+// Priority of the booleans
+// The first to be true will be printed. Else - Anonymous 
+console.log("Hello! " + (username || firstName || familiyName || "Anonymous!"))
+
 
 // Challenge
+// Extra - Using time function
 setTimeout(function(){ alert("1"); alert("2"); alert("3") }, 3000);
+
+// Using Only OR. (The solution)
 alert(1) || alert(2) || alert(3)   
--- Conditionals(If_Else)/Advanced_Conditonal_Lights.js
+++ Conditionals(If_Else)/Advanced_Conditonal_Lights.js
@@ -1,4 +1,10 @@
-let light = prompt("What is the traffic light color?")
+// 1.  כתבו תוכנית המדמה רמזור.
+// קלטו צבע והדפיסו בהתאם .
+
+
+
+let light = prompt("What is the traffic light color?");
+
 // classic way
 if (light == "red") {
     console.log("You shall not pass!!")
@@ -9,10 +15,35 @@ if (light == "green") {
 if (light == "orange") {
     console.log("Go quick, or stop!")
 }
-// short statment 
-let light = prompt("What is the traffic light color?")
+
+// short statment - Short IF
+let light = prompt("What is the traffic light color?");
 let output = (light == "red") ? "not pass!" : (light == "green") ? "Go!" : (light == "orange") ? "get_ready" : "Wrong word";
 console.log(output)
 
+// 2. כתבו תוכנית המדמה חיישן מהירות.
+// קלטו מהירות של המכונית והדפיסו:
+// עד 40 - סע כבר!
+// עד 80 - נסיעה טובה!
+// עד 120 - האט!
+// מעל - עצור מיד!
 
-// ctrl + ? to comment out
+let speed = 120;
+
+let speed_result = (speed <= 40) ? "GO GO!" : 
+(speed > 40 && speed <= 80) ? "Have a good ride" :
+(speed > 80 && speed <= 120) ? "Slow down!" : 
+(speed >= 120) ? "Stop Now!" 
+: "Wrong speed";
+console.log(speed_result);
+
+let a = 7;
+let b = 4;
+let c = 3;
+
+
+let biggest = (a > b) && (a > c) ? `a : ${a} is the biggest` : (b > a) && (b > c) ? `b: ${b} is the biggest` : (c > b) && (c > a) ? `c :${c} is the biggest` : "Wrong number";
+console.log(biggest);
+
+// to be optimized : 
+// let msg = (a > b) ? (a > c) ? a : c : (b > c) ? b : c;
