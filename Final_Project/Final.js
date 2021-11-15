33fd5da95fd140fa07966f77e20dca83901aecd9
Emmanuel Ouzan <Emmanuel.ouzan@gmail.com>
2021-11-15 13:41:01 +0200

    Added_Temporary_HTML

--- Final_Project/Final.js
+++ Final_Project/Final.js
@@ -1,6 +1,6 @@
 // constructor of person 
 
-function Person(fname,lname,id,city,birthDate,parentId=0) {
+function Person(fname, lname, id, city, birthDate, parentId = 0) {
     this.fname = fname;
     this.lname = lname;
     this.id = id;
@@ -9,19 +9,30 @@ function Person(fname,lname,id,city,birthDate,parentId=0) {
     this.parentId = parentId;
 }
 
-let person1 = new Person('Emmanuel','Ouzan','123','JLM','08/08/1997','5');
+let person1 = new Person('Emmanuel', 'Ouzan', '123', 'JLM', '08/08/1997', '5');
 let people = [];
 people.push(person1);
+
 // people.forEach(person => console.log(person.id)  );
+
 let check = people.filter(person => console.log(person));
 
-function check_input(fname,lname,id,city,birthDate,parentId=0) {
-    
+function check_input(fname, lname, id, city, birthDate, parentId = 0) {
+    // check fname
+    // try and catch unexpected tokens
+        for (i in fname) {  
+            if (!isNaN(i)) {
+                console.log("Please enter an input without numbers");
+                return false;
+            }
+    }
 }
-function add_person(fname,lname,id,city,birthDate,parentId=0) {
-    if(parentId != 0){
-        if(people.forEach(person => console.log(person.id)){
-            
+
+function add_person(fname, lname, id, city, birthDate, parentId = 0) {
+    check_input(fname, lname, id, city, birthDate, parentId);
+    if (parentId != 0) {
+        if (people.forEach(person => console.log(person.id))) {
+
         }
     }
 }
