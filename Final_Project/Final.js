c2270c9f01a29b226e86f0c4b82ad899b63bf726
Emmanuel Ouzan <Emmanuel.ouzan@gmail.com>
2021-11-25 20:12:22 +0200

    Great

--- Final_Project/Final.js
+++ Final_Project/Final.js
@@ -2,37 +2,50 @@
 // Maybe return word instead of boolean in check input methods
 // How to catch unexpected tokens?
 
-// constructor of person 
-
-function Person(fname, lname, id, city, birthDate, parentId = 0) {
-    this.fname = fname;
-    this.lname = lname;
-    this.id = id;
-    this.city = city;
-    this.birthDate = birthDate;
-    this.parentId = parentId;
+// Creation of object person
+function Person_creation(fname, lname, id, city, birthDate, parentId = 0) {
+    let person = {
+        fname: fname,
+        lname: lname,
+        id: id,
+        city: city,
+        birthDate: birthDate,
+        parentid: parentId
+    };
+    return person;
 }
-
-let person1 = new Person('Emmanuel', 'Ouzan', '123', 'JLM', '08/08/1997', '5');
+// Creation Example 
+let person1 = Person_creation('Emmanuel', 'Ouzan', '123', 'JLM', '08/08/1997', '0');
+let person2 = Person_creation('Aviad', 'derli', '32432011', 'JLM', '08/02/1989', '0');
+// Change of attribute
+person2.city = "TLV";
+// Creation of an array of objects called people
 let people = [];
 people.push(person1);
+people.push(person2);
+// Operation on each of one of the people
+people.forEach(v => console.log(v.birthDate))
+people.forEach(person => console.log(person.id));
 
-// people.forEach(person => console.log(person.id)  );
+// Filters for something
+// last name = Ouzan,  2. Where Emmanuel Exists
+let check = people.filter(person => person.lname == 'Ouzan');
+people.filter(element => element == 'Emmanuel');
 
-let check = people.filter(person => console.log(person));
 
 function check_Numbers(user_input) {
-    for (i in user_input) {  
+    for (i in user_input) {
         if (!isNaN(i)) {
             console.log("Please enter an input without numbers");
             // Maybe return string
             return false;
         }
+    }
 }
-}
+
 function check_for_letters(user_input) {
-    for(i in user_input){
-        if(isNaN(i)){
+    for (i in user_input) {
+        if (isNaN(i)) {
             console.log("Please enter an input without letters");
             return false;
         }
@@ -40,7 +53,7 @@ function check_for_letters(user_input) {
 }
 
 function check_input(fname, lname, id, city, birthDate, parentId = 0) {
-    
+
     // try and catch unexpected tokens
 }
 
