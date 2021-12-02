dd0149452f7a7194c3a2b16b6a7ef544adc5d9b1
EmmanuelOuzan <Emmanuel.ouzan@gmail.com>
2021-12-02 16:15:06 +0200

    Better_Final

--- Final_Project/Final.js
+++ Final_Project/Final.js
@@ -3,7 +3,6 @@
 // How to catch unexpected tokens?
 // Check if there a token
 // Check if string is not empty
-// check git
 
 
 
@@ -19,24 +18,53 @@ function Person_creation(fname, lname, id, city, birthDate, parentId = 0) {
     };
     return person;
 }
+
 // Creation Example 
-let person1 = Person_creation('Emmanuel', 'Ouzan', '123', 'JLM', '08/08/1997', '0');
-let person2 = Person_creation('Aviad', 'derli', '32432011', 'JLM', '08/02/1989', '0');
+let person1 = Person_creation('Emmanuel', 'Ouzan', '324320134', 'JLM', '08/08/1997', '0');
+let person2 = Person_creation('Aviad', 'derli', '123', 'JLM', '08/02/1989', '0');
+let person3 = Person_creation("Hana", 'lname', '321', 'BeitShemesh', '08/06/97', 324320134);
+
 // Change of attribute
 person2.city = "TLV";
+
 // Creation of an array of objects called people
 let people = [];
+// add the people to the array
 people.push(person1);
 people.push(person2);
+
 // Operation on each of one of the people
 people.forEach(v => console.log(v.birthDate));
 people.forEach(person => console.log(person.id));
+
+
 
 // Filters for something
 // last name = Ouzan,  2. Where Emmanuel Exists
 let check = people.filter(person => person.lname == 'Ouzan');
 people.filter(element => element == 'Emmanuel');
 
+function add_person(params) {
+    let fname = prompt("Please enter your first name");
+    let lname = prompt("Please enter your last name");
+}
+function check_input(fname, lname, id, city, birthDate, parentId = 0) {
+    validate_String(fname);
+    validate_String(lname);
+    validate_Number(id);
+    validate_String(city);
+    validate_date(birthDate);
+    validate_Number(parentId);
+}
+
+function add_person(fname, lname, id, city, birthDate, parentId = 0) {
+    check_input(fname, lname, id, city, birthDate, parentId);
+    if (parentId != 0) {
+        if (people.forEach(person => console.log(person.id))) {
+
+        }
+    }
+}
 
 function validate_Number(user_input) {
     for (i of user_input) {
@@ -58,22 +86,17 @@ function validate_String(user_input) {
     }
     return true;
 }
+function validate_date(date) {
+    // TO DO 
 
-function check_input(fname, lname, id, city, birthDate, parentId = 0) {
-    validate_String(fname);
-    validate_String(lname);
-    validate_Number(id);
-    validate_String(city);
-    // birthdate which format? 08/08/1a97 dd/mm/
-    //const date1 = new Date('December 17, 1995 03:24:00');
-    validate_Number(parentId);
 }
 
-function add_person(fname, lname, id, city, birthDate, parentId = 0) {
-    check_input(fname, lname, id, city, birthDate, parentId);
-    if (parentId != 0) {
-        if (people.forEach(person => console.log(person.id))) {
-
-        }
+function check_for_parent(parentId) {
+    if (people.find(person => person.id == parentId)) {
+        console.log("Parent found!");
+    }
+    else {
+        console.log("No parent exists with that ID");
     }
 }
+check_for_parent('123')
