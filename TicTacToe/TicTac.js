02cb7b3abc3efee6b49f43431d050b7d4edda1c0
EmmanuelOuzan <Emmanuel.ouzan@gmail.com>
2021-12-06 19:15:04 +0200

    With_events and better game

--- TicTacToe/TicTac.js
+++ TicTacToe/TicTac.js
@@ -1,18 +1,39 @@
+// Creating the board
 let board = [];
 
+// Looking at the possible wins.
 const combinations = [
-    [0, 1, 2],
+    // rows
+    [0, 1, 2], 
     [3, 4, 5],
     [6, 7, 8],
+    // columes
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
+    // diagonal line
     [2, 4, 6],
     [0, 4, 8],
 ]
+// Creation of the players
+let player1 = {
+    name: 'first_player',
+    shape: 'X',
+    style: "css_x"
+}
+let player2 = {
+    name: 'second_player',
+    shape: 'O',
+    style: "css_o"
+}
 
-let boardGame = document.getElementById('board')
+// Targeting the board as an element and keeping it for quicker reference
+let boardGame = document.getElementById('board');
+
+// Creating a listener for the board. So when I click it , it will react.
 boardGame.addEventListener('click', clickOnCell);
+
+// an example player
 let currentPlayer = {
     name: "player 1",
     shape: "X",
@@ -20,7 +41,7 @@ let currentPlayer = {
 }
 
 function clickOnCell(e) {
-    // check witch cell pressed
+    // check which cell pressed
     let index = Number(e.target.id.slice(4))
     // if exist
     board[index] = currentPlayer.shape;
